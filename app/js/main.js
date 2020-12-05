a2dcc5f57c9e31ae223f00ff69319c6c3fbf7002
dobpo1 <dobpo1@yandex.ru>
2020-12-05 22:02:33 +0300

    added slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,14 @@
 $(function(){
-    var mixer = mixitup('.products__inner-box'); 
+    var mixer = mixitup('.products__inner-box');
+    $(".rate-star").rateYo({
+        rating: 5,
+        starWidth: "12px",
+        readOnly: true
+      });
+    $('.product-slider__inner').slick({
+        arrows: false,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        dots: true
+    });
 });
