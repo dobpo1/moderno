fe73e16185adb7ceb6f91bcf109d06ccc30124b4
dobpo1 <dobpo1@yandex.ru>
2021-09-02 18:43:02 +0300

    product-one

--- app/js/main.js
+++ app/js/main.js
@@ -12,6 +12,14 @@ $(function(){
         dots: true
     });
 });
+$('.product-one__tabs .tab').on('click', function(event) {
+    var id = $(this).attr('data-id');
+        $('.product-one__tabs').find('.tab-item').removeClass('active-tab').hide();
+        $('.product-one__tabs .tabs').find('.tab').removeClass('active');
+        $(this).addClass('active');
+        $('#'+id).addClass('active-tab').fadeIn();
+        return false;
+    });
 
 $('.menu__btn').on('click', function(){
     $('.menu__list').slideToggle();    
@@ -20,3 +28,4 @@ $('.menu__btn').on('click', function(){
 $('.header__btn-menu').on('click', function(){
     $('.header__box').toggleClass('active');
 });
+
